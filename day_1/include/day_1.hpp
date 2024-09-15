03dc7cb6b596af81b39234f5a9945e062910fc1b
Awadhy Mohammed <awadhym@outlook.com>
2024-09-15 21:48:03 +0100

    Final iteration of solution to part 1 puzzle for day 1
    Use of std::views means that are seperate vector is not
    required to be created.

--- day_1/include/day_1.hpp
+++ day_1/include/day_1.hpp
@@ -5,5 +5,4 @@
 namespace part_1 {
 uint32_t
 calculate_calibration_values(std::vector<std::string> const &file_contents);
-std::vector<uint8_t> get_numbers_found_in_string(std::string const &line);
 } // namespace part_1
--- day_1/source/day_1.cpp
+++ day_1/source/day_1.cpp
@@ -1,24 +1,16 @@
 #include "day_1.hpp"
-
-std::vector<uint8_t>
-part_1::get_numbers_found_in_string(std::string const &line) {
-  std::vector<uint8_t> numbers;
-
-  for (const auto &c : line) {
-    if (std::isdigit(c)) {
-      numbers.push_back(c - '0');
-    }
-  }
-  return numbers;
-}
+#include <ranges>
 
 uint32_t part_1::calculate_calibration_values(
     std::vector<std::string> const &file_contents) {
   uint32_t sum = 0;
   for (std::string const &line : file_contents) {
-    std::vector<uint8_t> numbers = get_numbers_found_in_string(line);
-    uint8_t first_digit = numbers.front();
-    uint8_t second_digit = numbers.back();
+    auto numbers{std::views::filter(line, [](char c) {
+      if (std::isdigit(c))
+        return c;
+    })};
+    uint8_t first_digit = numbers.front() - '0';
+    uint8_t second_digit = numbers.back() - '0';
     uint8_t double_digit = (first_digit * 10) + second_digit;
     sum += double_digit;
   }
