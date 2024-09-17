8321491fd50a4d4080faf8f906e5a8f816fa9907
Awadhy Mohammed <awadhym@outlook.com>
2024-09-17 09:50:09 +0100

    First iteration to solution of part_2 for puzzle_1
    Uses a sorting window algorithm

--- day_1/include/day_1.hpp
+++ day_1/include/day_1.hpp
@@ -1,3 +1,4 @@
+#include <array>
 #include <fstream>
 #include <iostream>
 #include <vector>
@@ -6,3 +7,13 @@ namespace part_1 {
 uint32_t
 calculate_calibration_values(std::vector<std::string> const &file_contents);
 } // namespace part_1
+
+namespace part_2 {
+
+extern std::array<std::string, 10> words;
+
+uint32_t
+calculate_calibration_values(std::vector<std::string> const &file_contents);
+bool is_substring_of_word(const std::string &str);
+bool is_number(const std::string &word);
+} // namespace part_2
